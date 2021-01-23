6e43ee81bb51b06c9e5ed067b751f2c77124d7d7
byznrdmrr <byznrrdmr@gmail.com>
2021-01-23 22:12:06 +0300

    created router.js

--- src/router.js
+++ src/router.js
@@ -2,3 +2,26 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import auth from '@react-native-firebase/auth';
+
+const Stack = createStackNavigator();
+function AuthStack() {
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="Login" />
+            <Stack.Screen name="Sign"/>
+            <Stack.Screen name="Home" component={Router}/>
+
+        </Stack.Navigator>
+    )   
+}
+function Router() {
+    return(
+    <NavigationContainer>
+        <Tab.Navigator>
+            <Stack.Screen name="Auth" component={AuthStack}/>
+            <Stack.Screen name="Todo" component={Posts}/>
+        </Tab.Navigator>
+    </NavigationContainer>
+    );
+}
+export default Router;
