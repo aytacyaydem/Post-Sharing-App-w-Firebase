0652b62b9e3510029161013e3518664a896852f2
byznrdmrr <byznrrdmr@gmail.com>
2021-01-25 18:18:39 +0300

    edited login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,43 +1,35 @@
 import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  Alert,
-} from 'react-native';
+import {View, Text, TextInput, Alert} from 'react-native';
 import {login_page_styles} from '../styles/page_styles';
 import auth from '@react-native-firebase/auth';
-
 
 import {CustomButton} from '../components/Button';
 
 const Login = ({navigation}) => {
+  // auth().signOut();
+
   const [email, setEmail] = useState('');
   const [password, setPssword] = useState('');
-  const [error, setError] = useState({});
+  // const [error, setError] = useState({});
   function signIn() {
-    auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(() => navigation.navigate('Home'))
-      .catch(({code, message}) => Alert.alert(code, message));
-
-    // .catch(({code}) => {
-    //   switch (code) {
-    //     case 'auth/no-current-user':
-    //       setError('Lütfen ilgili alanları doldurun.');
-    //       break;
-
-    //     default:
-    //       setError({unexpected: 'Beklenmeyen bir hata oluştu'});
-    //       break;
-    //   }
-    // });
+    if (email === '') {
+      Alert.alert('Email alanı boş bırakılamaz');
+    } else if (password === '') {
+      Alert.alert('Password alanı boş bırakılamaz');
+    } else if (email && password === '') {
+      Alert.alert('Email ve password alanı boş bırakılamaz');
+    } else {
+      auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => navigation.navigate('Home'))
+        .catch(({code, message}) => Alert.alert(code, message));
+    }
   }
   return (
     <View style={login_page_styles.container}>
       <Text style={login_page_styles.headerText}>Login Screen</Text>
       <View style={login_page_styles.formContainer}>
-        {error ? <Text> {Object.values(error)} </Text> : null}
+        {/* {error ? <Text> {Object.values(error)} </Text> : null} */}
         <View style={login_page_styles.inputContainer}>
           <TextInput
             autoCapitalize="none"
